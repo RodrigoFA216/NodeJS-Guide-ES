c9f1f1c897cc344ed05ea46aa75b28a769b8bb38
RodrigoFA216 <rfloresa1900@alumno.ipn.mx>
2022-04-11 01:04:35 -0500

    Fix primero.js

--- primero.js
+++ primero.js
@@ -1,31 +1,3 @@
-//Tipos de variables
-//entero
-// var entero=2;
-// var decimal=2.0;
-// var texto="Alfred";
-// var bool=false;
-// let a=12;
-// let b=24;
-// let c=25;
-// let d=92;
-// let e=91;
-// let f=true;
-// let g=false
-// let resultado1=(a<b||b<c)&&(!a&&e!=d);
-// document.write(resultado1)
-// let numero;
-// alert(numero);
-
-// let numero2=null;
-// alert(numero2);
-
-// let variable=prompt("text");
-// alert("el texto introducido es " + variable);
-
-// var nombre="Rodrigo Flores";
-// var frase=`soy ' "${nombre}" ' y estoy caminando`;
-// document.write(frase);
-
 // Listos-----------------------------------------------------------------------------
 //DECLARACIÓN DE VARIABLES
 const pi = 3.14159265359
